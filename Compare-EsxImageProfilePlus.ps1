dfd7a4dcb96c4a625344560dedc0bf5d32ddf549
Christophe Calvet <christophe.calvet@gmail.com>
2017-08-31 01:05:29 +0200

    Add first version of the two functions

---
+++ Compare-EsxImageProfilePlus.ps1
@@ -0,0 +1,117 @@
+﻿function Compare-EsxImageProfilePlus{
+<#
+.SYNOPSIS
+Compare two EsxImageProfile.
+Based on "Compare-EsxImageProfile" but with a simpler output.
+
+.DESCRIPTION
+Compare two EsxImageProfile.
+Based on "Compare-EsxImageProfile" but with a simpler output.
+
+.NOTES
+Author: Christophe Calvet
+Blog: http://www.thecrazyconsultant.com/
+
+.PARAMETER ReferenceProfile
+Specifies the reference image profile for comparison.
+
+.PARAMETER ComparisonProfile
+The image profile to compare against.
+
+.PARAMETER IncludeVibInOutput
+Add the Vib in the output table. Only useful if extra information need to be extracted.
+
+.EXAMPLE
+#The standard output list Vib Name, Vendor And Version for both the source and destination profile, and the analysis.
+Compare-EsxImageProfilePlus $ImageProfile1 $ImageProfile2 | select * | ogv
+
+#The switch IncludeVibInOutput is used to extra more information like CreationDate or Description in the example below.
+Compare-EsxImageProfilePlus $ImageProfile1 $ImageProfile2 -IncludeVibInOutput | select Name,VendorRef,VersionRef,@{N="CreationDateRef";E={$_.VibRef.CreationDate}},@{N="DescriptionRef";E={$_.VibRef.Description}},VendorComp,VersionComp,@{N="CreationDateComp";E={$_.VibComp.CreationDate}},@{N="DescriptionComp";E={$_.VibComp.Description}},Analysis | ogv
+#>
+	[CmdletBinding()]
+	param(
+	[Parameter(Mandatory=$true)]
+	[VMware.ImageBuilder.Types.ImageProfile]$ReferenceProfile,
+	[Parameter(Mandatory=$true)]
+	[VMware.ImageBuilder.Types.ImageProfile]$ComparisonProfile,
+	[switch]$IncludeVibInOutput
+	)
+	process{
+
+$ViblistReferenceProfile = ($ReferenceProfile.viblist)
+$ViblistComparisonProfile = ($ComparisonProfile.viblist)
+
+	
+	$ListOfAllVib  = $ViblistReferenceProfile + $ViblistComparisonProfile
+
+	$CompareResult = Compare-EsxImageProfile $ReferenceProfile $ComparisonProfile
+	$UpgradeFromRef = $CompareResult.UpgradeFromRef
+	$DowngradeFromRef = $CompareResult.DowngradeFromRef
+
+	$VibNameUpgradeFromRef = $UpgradeFromRef | foreach-object{
+	$VibName = $_ -replace ('.*bootbank_','') -replace ('.*re_locker_','') -replace ('_.*','')
+	$VibName
+	}
+
+	$VibNameDowngradeFromRef = $DowngradeFromRef | foreach-object{
+	$VibName = $_ -replace ('.*bootbank_','') -replace ('.*re_locker_','') -replace ('_.*','')
+	$VibName
+	}
+
+	$ListOfAllVib | select -unique Name | foreach-object {
+		$Name = $_.Name
+		$Analysis = "NotTested"
+		$VibRef = ($ViblistReferenceProfile | where {$_.Name -eq $Name})
+		$VibComp = ($ViblistComparisonProfile | where {$_.Name -eq $Name})
+
+		If (($VibRef -and !$VibComp )){
+		$Analysis = "OnlyInRef"
+		}
+		If ((!$VibRef -and $VibComp )){
+		$Analysis = "OnlyInComp"
+		}
+
+		If (($VibRef -and $VibComp)){
+			If (($VibRef.version) -eq ($VibComp.version)){
+			$Analysis = "Identical"
+			}
+			if ($Name -in $VibNameUpgradeFromRef){
+			$Analysis = "UpgradeFromRef"
+			}
+			if ($Name -in $VibNameDowngradeFromRef){
+			$Analysis = "DowngradeFromRef"
+			}	
+		}
+
+
+								if($IncludeVibInOutput){
+									$Result = New-Object -Type PSObject -Prop ([ordered]@{
+									'Name' = $Name
+									'VendorRef' = $VibRef.Vendor
+									'VersionRef' = $VibRef.Version
+									'VibRef' = $Vibref
+									'VendorComp' = $VibComp.Vendor
+									'VersionComp' = $VibComp.Version
+									'VibComp' = $VibComp
+									'Analysis' = $Analysis	
+									 })    
+									Return $Result
+								}
+								Else{
+									$Result = New-Object -Type PSObject -Prop ([ordered]@{
+									'Name' = $Name
+									'VendorRef' = $VibRef.Vendor
+									'VersionRef' = $VibRef.Version
+									'VendorComp' = $VibComp.Vendor
+									'VersionComp' = $VibComp.Version								
+									'Analysis' = $Analysis									
+									 })    
+									Return $Result								
+								}
+				
+		}
+	
+	
+	}
+
+}
---
+++ Merge-EsxImageProfile.ps1
@@ -0,0 +1,76 @@
+﻿function Merge-EsxImageProfile{
+<#
+.SYNOPSIS
+Create a new EsxImageProfile by merging two EsxImageProfile and keep highest vib.
+
+.DESCRIPTION
+Create a new EsxImageProfile by merging two EsxImageProfile and keep highest vib.
+Based on "New-EsxImageProfile"
+Requirement: Compare-EsxImageProfilePlus
+
+.NOTES
+Author: Christophe Calvet
+Blog: http://www.thecrazyconsultant.com/
+
+.PARAMETER Profile1
+First Image profile
+
+.PARAMETER Profile2
+Second Image Profile
+
+.EXAMPLE
+$NewProfile = Merge-EsxImageProfile $Profile1 $Profile2 -AcceptanceLevel "PartnerSupported" -OverrideProfileWithSameName
+
+#>
+	[CmdletBinding()]
+	param(
+	[Parameter(Mandatory=$true)]
+	[VMware.ImageBuilder.Types.ImageProfile]$Profile1,
+	[Parameter(Mandatory=$true)]
+	[VMware.ImageBuilder.Types.ImageProfile]$Profile2,
+	$NewName,
+	$NewVendor,
+	$NewDescription,
+	[ValidateSet('VMwareCertified','VMwareAccepted','PartnerSupported','CommunitySupported')] 
+	[string]$AcceptanceLevel = "VMwareCertified",
+	[switch]$OverrideProfileWithSameName		
+	)
+	process{
+		if(!$NewName){
+		$NewName = 	"$($Profile1.name)" + "___" + "$($Profile2.name)"
+		}
+		if(!$NewVendor){
+		$NewVendor = "$($Profile1.vendor)" + "___" + "$($Profile2.vendor)"
+		}
+		if(!$NewDescription){
+		$NewDescription	= "$($Profile1.Description)" + "___" + "$($Profile2.Description)"
+		}				
+		if($OverrideProfileWithSameName){
+		get-EsxImageProfile -name $NewName | Remove-EsxImageProfile
+		}
+		
+		#Put latest VIB in one array $SoftwarePackage
+		$SoftwarePackage = @()	
+
+		$CompareResult = Compare-EsxImageProfilePlus $Profile1 $Profile2 -IncludeVibInOutput
+			$CompareResult | foreach-object{
+			$CurrentVib = $_
+			$Analysis = $_.Analysis
+			$Name = $_.name
+				switch($Analysis){
+				OnlyInRef{$SoftwarePackage += $CurrentVib.VibRef}
+				OnlyInComp{$SoftwarePackage += $CurrentVib.VibComp}
+				Identical{$SoftwarePackage += $CurrentVib.VibRef}
+				UpgradeFromRef{$SoftwarePackage += $CurrentVib.VibComp}
+				DowngradeFromRef{$SoftwarePackage += $CurrentVib.VibRef}
+				Default{Write-Error "The output of Compare-EsxImageProfilePlus is wrong"}
+				}
+			
+			}
+
+		#Build new profile
+		New-EsxImageProfile -NewProfile -name $NewName -vendor $NewVendor -SoftwarePackage $SoftwarePackage -AcceptanceLevel $AcceptanceLevel -Description $NewDescription
+		
+		
+	}
+}
